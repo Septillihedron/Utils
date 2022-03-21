e1a94d513501c6ce1bd0caeed5862d2440355ec1
Septillihedron <resha3adnandaru@gmail.com>
2022-03-21 22:20:12 +0700

    Initial commit

---
+++ src/main/java/me/sepdron/utils/CollectionUtils.java
@@ -0,0 +1,38 @@
+package me.sepdron.utils;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class CollectionUtils {
+
+	private CollectionUtils() {}
+
+	public static <K, V> Map<K, V> getMapAs(Map<?, ?> unconvertedMap, Class<K> keyClass, Class<V> valueClass) {
+		Map<K, V> map = new HashMap<K, V>();
+		unconvertedMap.forEach((key, val) -> {
+			if(keyClass.isInstance(key) && valueClass.isInstance(val))
+				map.put(keyClass.cast(key), valueClass.cast(val));
+		});
+		return Map.copyOf(map);
+	}
+	public static <K, V> Map<K, V> getAsMap(Object object, Class<K> keyClass, Class<V> valueClass) {
+		if (!(object instanceof Map<?, ?>)) return null;
+		return getMapAs((Map<?, ?>) object, keyClass, valueClass);
+	}
+
+	public static <T> List<T> getListAs(List<?> unconvertedList, Class<T> itemClass) {
+		List<T> list = new ArrayList<T>();
+		unconvertedList.forEach((item) -> {
+			if (itemClass.isInstance(item))
+				list.add(itemClass.cast(item));
+		});
+		return List.copyOf(list);
+	}
+	public static <T> List<T> getAsList(Object object, Class<T> itemClass) {
+		if (!(object instanceof List<?>)) return null;
+		return getListAs((List<?>) object, itemClass);
+	}
+
+}
